3842b17f56f9f0815249f277af6f9ea87261348f
Reza Haidari <shoosh2007@gmail.com>
2018-06-10 15:39:21 +0430

    add button element

---
+++ components/Auth/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <f-modal ref="modal"  />
+</template>
+
+<script>
+
+import FModal from '~/components/elements/modal'
+
+  export default {
+    components:{
+      FModal
+    },
+    methods:{
+      display(){
+        this.$refs.modal.display()
+      }
+    }
+  }
+</script>
--- components/elements/button/index.js
+++ components/elements/button/index.js
@@ -1,10 +1,17 @@
 import './button.styl'
 export default {
   functional: true,
+
   render(h, {props, ...rest}){
+    //console.log(rest)
+    //let self = this
+    // function click(){
+    //   self.$emit('onClick')
+    // }
+    console.log(rest)
     let classes = ['btn']
     let styles = props.styles ? props.styles.split(' ') : []
     styles.forEach(element => classes.push('btn-'+element));
-    return <button class={classes}> {rest.slots().default} </button>
+    return <button onClick={rest.data.on.onClick} class={classes}> {rest.slots().default} </button>
   }
 }
