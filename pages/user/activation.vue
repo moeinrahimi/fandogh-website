f6ab42e074eb96de5fbc81752c475d07d2a93d96
Reza Haidari <shoosh2007@gmail.com>
2018-06-27 15:12:16 +0430

    fix activation undefined query params

--- pages/user/activation.vue
+++ pages/user/activation.vue
@@ -6,7 +6,7 @@
     export default {
       beforeMount(){
         try {
-          this.$store.dispatch('activation', {code: route.query.code, id: route.query.user_id}).then(res => {
+          this.$store.dispatch('activation', {code: this.$route.query.code, id: this.$route.query.user_id}).then(res => {
             this.$store.dispatch('setMessage', res.message)
             this.$router.push('/')
           })
