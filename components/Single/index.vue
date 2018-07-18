563e44f1242048b6bd5fcd64d20ac4c2821495df
Reza Haidari <shoosh2007@gmail.com>
2018-07-18 20:16:38 +0430

    implement sidebar single page

---
+++ components/Single/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="single">
+        <div class="detail">
+            <slot></slot>
+        </div>
+        <div class="s-sidebar">
+            <sidebar> </sidebar>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Sidebar from './sidebar'
+    export default {
+      components:{
+        Sidebar
+      }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .single
+        margin-top 100px
+        display flex
+        flex-flow wrap row-reverse
+        align-items flex-start
+        .detail
+            width calc(100% - 300px)
+            margin-right 30px
+
+</style>
---
+++ components/Single/sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="single-sidebar">
+        <ul>
+            <li><router-link :to="{path:'about-us'}">درباره ما</router-link></li>
+            <li><router-link :to="{path:'/contact-us'}">تماس با ما</router-link></li>
+            <li><router-link :to="{path:'use-conditions'}">شرایط استفاده از خدمات</router-link></li>
+        </ul>
+    </div>
+</template>
+<style lang="stylus" scoped>
+    .single-sidebar
+        overflow hidden
+        width 270px
+        border-radius: 20px;
+        background-image: linear-gradient(to bottom, #2f1ea1, #0b003a);
+        box-shadow: -2px 0 6px 0 rgba(21, 0, 113, 0.36);
+        min-height 600px
+        ul
+            display block
+            margin-top 24px
+            li
+                a
+                    display block
+                    padding 12px 24px
+                    &.nuxt-link-active
+                        background-color #000
+                        border-right 2px solid #26c6ec
+
+</style>
