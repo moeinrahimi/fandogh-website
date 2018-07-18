a3e2ae2956f61aa90096591f736bff12b9eb3853
Reza Haidari <shoosh2007@gmail.com>
2018-07-18 23:47:34 +0430

    implement contact-us page

---
+++ plugins/google-map.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import * as VueGoogleMaps from '~/node_modules/vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDuJg2x9OKvkqcVyyWjvusnVbNustcNgB8',
+    libraries: 'places'
+  }
+})
