2a49ac4827858dd5701e58b8afa1c0c96be02540
Reza Haidari <shoosh2007@gmail.com>
2018-07-22 23:29:52 +0430

    catch error activation code

--- pages/user/activation.vue
+++ pages/user/activation.vue
@@ -5,15 +5,13 @@
 <script>
     export default {
       beforeMount(){
-        try {
           this.$store.dispatch('activation', {code: this.$route.query.code, id: this.$route.query.user_id}).then(res => {
             this.$store.dispatch('setMessage', res.message)
             this.$router.push('/')
+          }).catch(e => {
+            this.$store.dispatch('setMessage', e)
+            this.$router.push('/')
           })
-        } catch (e) {
-          this.$store.dispatch('setMessage', e)
-          this.$router.push('/')
-        }
       }
     }
 </script>
