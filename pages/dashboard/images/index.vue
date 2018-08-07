dbbd9df5765439f3c759ba7f743a0a7c3b94d635
Reza Haidari <shoosh2007@gmail.com>
2018-08-07 15:12:27 +0430

    [FAN-79] implement delete image

--- pages/dashboard/images/index.vue
+++ pages/dashboard/images/index.vue
@@ -3,7 +3,7 @@
         <div class="row-block">
             <f-button styles="red" @onClick="$router.push('/dashboard/images/create')" > افزودن ایمیج </f-button>
         </div>
-        <f-table title="ایمیج های شما" :header="header" :data="_images" :actions="[{title:`<img src='/icons/ic-time.svg'/><span> ورژن ها </span>`, action:'versions'}, {title:`<img src='/icons/ic-upload.svg' /> <span>آپلود</span> `, action:'createVersion'}, {title:`<img src='/icons/ic-delete.svg' /> <span>حذف</span> `, action:'versions'}]"></f-table>
+        <f-table title="ایمیج های شما" :header="header" :data="_images" :actions="[{title:`<img src='/icons/ic-time.svg'/><span> ورژن ها </span>`, action:'versions'}, {title:`<img src='/icons/ic-upload.svg' /> <span>آپلود</span> `, action:'createVersion'}, {title:`<img src='/icons/ic-delete.svg' /> <span>حذف</span> `, action:'delete'}]"></f-table>
     </div>
 </template>
 
@@ -11,6 +11,7 @@
     import FTable from '~/components/Dashboard/table'
     import FButton from '~/components/elements/button'
     import FDate from '~/utils/date'
+    import Alert from '../../../components/Dashboard/alert'
     export default {
       layout: 'dashboard',
       async asyncData({store, route, redirect}){
@@ -54,6 +55,26 @@
         },
         createVersion(id){
           this.$router.push(`/dashboard/versions/create/${id}`)
+        },
+        delete(id) {
+          this.$alertify({
+            title: ' آیا از حذف شدن ایمیج خود مطمئن هستید؟'
+          }, (status) =>{
+            if(status){
+              this.$store.dispatch('deleteImage', id, (res) => {
+                this.$store.dispatch('getImages')
+                this.$notify({
+                  title: res.message,
+                  type: 'success'
+                })
+              }).catch(e => {
+                this.$notify({
+                  title: e.message,
+                  type: 'error'
+                })
+              })
+            }
+          })
         }
       }
     }
