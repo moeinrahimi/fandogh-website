d98543423e19604aa18de0b4feec4049d011afe7
Reza Haidari <shoosh2007@gmail.com>
2018-08-12 13:37:58 +0430

    create first step domain page

--- pages/dashboard/domains/index.vue
+++ pages/dashboard/domains/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-
+  <div class="row">
+    <div class="col-md-6 col-xs-12" >
+      <h2>افزودن دامنه</h2>
+      <div class="fandogh-form-group">
+        <f-input v-model="name"  styles="input-white input-block input-dashboard" placeholder="نام دامنه را در این قسمت بنویسید"> </f-input>
+      </div>
+      <div class="fandogh-form-group margin-top-100">
+        <f-button styles="red block" v-if="!loading"  > مرحله بعد </f-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import FInput from '~/components/elements/input'
+  import FButton from '~/components/elements/button'
+  import FormValidator from '~/utils/formValidator'
   export default {
-    layout: 'dashboard'
+    layout: 'dashboard',
+    components:{
+      FInput,
+      FButton,
+      File
+    },
   }
 </script>
