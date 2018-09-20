ebe802ac5a44e954a36473e4fe47049e803e7ddb
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2018-09-20 15:01:44 +0200

    Update sidebar.vue

--- components/Header/sidebar.vue
+++ components/Header/sidebar.vue
@@ -23,7 +23,7 @@
                     <a @click="toggleMenu" href="#prices"> سرویس ها</a>
                 </li>
                 <li>
-                    <a rel="noopener" target="_blank" href="http://blog.fandogh.cloud/guide/">
+                    <a rel="noopener" target="_blank" href="https://docs.fandogh.cloud">
                         مستندات
                     </a>
                 </li>
