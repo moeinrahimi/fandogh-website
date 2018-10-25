7c4a420910ecf5c58fe8e27ef97f9b1a46af1bf6
moeinrahimi <imoeinrahimi1@gmail.com>
2018-10-25 18:03:58 +0330

    add plan page in dashboard

---
+++ components/Dashboard/circle-progress-bar/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="flex-wrapper">
+  <div class="single-chart">
+    <svg viewBox="0 0 36 36" class="circular-chart orange">
+        <defs>
+    <linearGradient id="lgrad" x1="0%" y1="100%" x2="100%" y2="0%" > 
+<stop offset="0%" style="stop-color:rgb(254,166,172);stop-opacity:1" />
+<stop offset="100%" style="stop-color:rgb(248,96,142);stop-opacity:1" />
+</linearGradient> 
+
+  </defs>
+
+      <path class="circle-bg" fill="none"
+        d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+      />
+      <path class="circle" stroke="url(#lgrad)"
+        :stroke-dasharray="percent+', 100'"
+        d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+      />
+      <text x="18" y="20.35" class="percentage">{{percent}}%</text>
+    </svg>
+  </div>
+  
+</div>
+</template>
+
+<script>
+  export default {
+    props : {
+      percent : {
+        type : String , 
+        default : 60
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+.flex-wrapper {
+  display: flex;
+  width 100%
+  justify-content: center;
+  align-items: center;
+}
+
+.single-chart {
+  width: 50%;
+  justify-content: space-around ;
+}
+
+.circular-chart {
+  display: block;
+  margin: 10px auto;
+  max-width: 80%;
+  max-height: 250px;
+}
+
+.circle-bg {
+  fill: none;
+  stroke: #eee;
+  stroke-width: 4;
+}
+
+.circle {
+  fill: none;
+  stroke-linecap: square;
+  stroke-width: 3.8;
+  animation: progress 1s ease-out forwards;
+}
+
+@keyframes progress {
+  0% {
+    stroke-dasharray: 0 100;
+  }
+}
+
+.circular-chart.orange .circle {
+}
+
+
+.percentage {
+  fill: #666;
+  font-family: sans-serif;
+  font-size: 0.5em;
+  text-anchor: middle;
+}
+</style>
---
+++ components/Dashboard/service/planConfig.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="plan-config">
+              <div class="plan-config-icon">
+                <img :src="'/icons/plans/' + icon" alt="">
+              </div>
+              <p class="plan-config-title">{{title}}</p>
+              <p>{{value}}</p>
+            </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: "cpu.png"
+    },
+    title: {
+      type: String,
+      default: "RAM"
+    },
+    value: {
+      type: String,
+      default: "2GB"
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.plan-config {
+  display: flex;
+  flex-direction: column;
+  padding: 27px 6px;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.plan-config-icon {
+  padding: 10px;
+}
+</style>
