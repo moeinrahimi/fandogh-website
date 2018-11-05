12a6632dab2e075d9c7ec181152e5eaf4c163e6c
moeinrahimi <imoeinrahimi1@gmail.com>
2018-11-05 17:17:32 +0330

    plan box changes

--- pages/dashboard/plans/_id.vue
+++ pages/dashboard/plans/_id.vue
@@ -101,92 +101,153 @@ export default {
       let price = this.$store.state.plan.plan.price;
       console.log(price, "a");
 
-      let price1 = parseFloat(price.replace(/,/g, ""))
+      let price1 = parseFloat(price.replace(/,/g, ""));
       let tax = 135000;
       let finalPrice = price1 + tax;
 
-      return finalPrice
+      return finalPrice;
     }
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-.plan
-    padding 45px
-    background #ffffff
-.plan-heading
-    font-size 18px
-    @media (max-width: 490px)
-        font-size 16px
-.plan-details
-    display flex
-    flex-direction column
-    p
-        margin-bottom 25px
-        font-weight bold
-        font-size 16px
-        line-height 1.75
-        @media (max-width: 490px)
-            font-size 14px
-.plan-box
-    max-width 100%
-.plan-inner
-    padding 85px
-    border-radius 10px
-    background-color #ffffff
-    box-shadow 0 0 6px 0 rgba(0, 0, 0, 0.16)
-    @media (max-width: 490px)
-        padding 50px
-.plan-heading
-    display flex
-    flex-direction column
-    justify-content center
-    align-items center
-    h3
-        font-size 18px
-        @media (max-width: 490px)
-            font-size 14px
-.plan-divider
-    height 2px
-    background-color #e7e8ea
-.plan-config
-    display flex
-    flex-direction row
-    justify-content space-around
-    align-items center
-    margin-top 25px
-.plan-information
-    display flex
-    flex-direction column
-    justify-content space-around
-    align-items center
-    font-size 16px
-    @media (max-width: 490px)
-        font-size 12px
-.plan-price
-    margin-top 60px
-    border solid 1px #b5b5b5
-    border-radius 10px
-    background-color #f0f4f8
-    text-align center
-.plan-h3
-    font-size 20px
-    line-height 1.75
-    @media (max-width: 490px)
-        font-size 16px
-.plan-payment
-    padding 32px
-    @media (max-width: 490px)
-        font-size 14px
-.back-btn
-    display flex
-.plan-name
-    font-family 'Roboto'
-.plan-info
-    font-weight bold
-    font-family 'Roboto'
-.plan-image img
-    @media (max-width: 490px)
-        width 24px
+.plan {
+    padding: 45px;
+    background: #ffffff;
+}
+
+.plan-heading {
+    font-size: 18px;
+
+    @media (max-width: 490px) {
+        font-size: 16px;
+    }
+}
+
+.plan-details {
+    display: flex;
+    flex-direction: column;
+
+    p {
+        margin-bottom: 25px;
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 1.75;
+
+        @media (max-width: 490px) {
+            font-size: 14px;
+        }
+    }
+}
+
+.plan-box {
+    max-width: 100%;
+    display: inline-block;
+}
+
+.plan-inner {
+    padding: 60px 50px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.16);
+
+    @media (max-width: 490px) {
+        padding: 50px;
+    }
+}
+
+.plan-heading {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h3 {
+        font-size: 18px;
+
+        @media (max-width: 490px) {
+            font-size: 14px;
+        }
+    }
+}
+
+.plan-divider {
+    height: 2px;
+    background-color: #e7e8ea;
+}
+
+.plan-config {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 25px;
+}
+
+.plan-information {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 16px;
+
+    @media (max-width: 490px) {
+        font-size: 12px;
+    }
+}
+
+.plan-information>span+span {
+    margin-top: 10px;
+}
+
+.plan-information+.plan-information {
+    margin-right: 10px;
+}
+
+.plan-image img {
+    width: 30px;
+
+    @media (max-width: 490px) {
+        width: 24px;
+    }
+}
+
+.plan-price {
+    margin-top: 60px;
+    border: solid 1px #b5b5b5;
+    border-radius: 10px;
+    background-color: #f0f4f8;
+    text-align: center;
+}
+
+.plan-h3 {
+    font-size: 20px;
+    line-height: 1.75;
+
+    @media (max-width: 490px) {
+        font-size: 16px;
+    }
+}
+
+.plan-payment {
+    padding: 32px;
+
+    @media (max-width: 490px) {
+        font-size: 14px;
+    }
+}
+
+.back-btn {
+    display: flex;
+}
+
+.plan-name {
+    font-family: 'Roboto';
+}
+
+.plan-info {
+    font-weight: bold;
+    font-family: 'Roboto';
+}
 </style>
