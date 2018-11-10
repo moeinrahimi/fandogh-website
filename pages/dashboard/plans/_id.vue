ba02c3fa1d1d2acd33f776242d717f7cb268c70f
moeinrahimi <imoeinrahimi1@gmail.com>
2018-11-10 22:25:51 +0330

    using const instead of let in plan page

--- pages/dashboard/plans/_id.vue
+++ pages/dashboard/plans/_id.vue
@@ -89,7 +89,7 @@ export default {
   data() {
     return {
       plan: {},
-      configs: [],
+      configs: []
     };
   },
   mounted() {
@@ -98,159 +98,104 @@ export default {
   },
   computed: {
     addTaxToPrice: function() {
-        let price = this.$store.state.plan.plan.price;
-      let priceRaw = parseFloat(price.replace(/,/g, ""));
-      let tax = Math.ceil((9 * priceRaw)) / 100;
-      let taxedPrice = tax+priceRaw
-      return taxedPrice.toLocaleString('fa-EG');
+      const price = this.$store.state.plan.plan.price;
+      const priceRaw = parseFloat(price.replace(/,/g, ""));
+      const tax = Math.ceil(9 * priceRaw) / 100;
+      const taxedPrice = tax + priceRaw;
+      return taxedPrice.toLocaleString("fa-EG");
     },
     tax: function() {
-      let price = this.$store.state.plan.plan.price;
-      let priceInt = parseFloat(price.replace(/,/g, ""));
-      let finalPrice = Math.ceil((9 * priceInt)) / 100;
-      return finalPrice.toLocaleString('fa-EG');
+      const price = this.$store.state.plan.plan.price;
+      const priceInt = parseFloat(price.replace(/,/g, ""));
+      const finalPrice = Math.ceil(9 * priceInt) / 100;
+      return finalPrice.toLocaleString("fa-EG");
     }
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-.plan {
-    padding: 45px;
-    background: #ffffff;
-}
-
-.plan-heading {
-    font-size: 18px;
-
-    @media (max-width: 490px) {
-        font-size: 16px;
-    }
-}
-
-.plan-details {
-    display: flex;
-    flex-direction: column;
-
-    p {
-        margin-bottom: 25px;
-        font-weight: bold;
-        font-size: 16px;
-        line-height: 1.75;
-
-        @media (max-width: 490px) {
-            font-size: 14px;
-        }
-    }
-}
-
-.plan-box {
-    max-width: 100%;
-    display: inline-block;
-}
-
-.plan-inner {
-    padding: 60px 50px;
-    border-radius: 10px;
-    background-color: #ffffff;
-    box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.16);
-
-    @media (max-width: 490px) {
-        padding: 50px;
-    }
-}
-
-.plan-heading {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    h3 {
-        font-size: 18px;
-
-        @media (max-width: 490px) {
-            font-size: 14px;
-        }
-    }
-}
-
-.plan-divider {
-    height: 2px;
-    background-color: #e7e8ea;
-}
-
-.plan-config {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    margin-top: 25px;
-}
-
-.plan-information {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    font-size: 16px;
-
-    @media (max-width: 490px) {
-        font-size: 12px;
-    }
-}
-
-.plan-information>span+span {
-    margin-top: 10px;
-}
-
-.plan-information+.plan-information {
-    margin-right: 10px;
-}
-
-.plan-image img {
-    width: 30px;
-
-    @media (max-width: 490px) {
-        width: 24px;
-    }
-}
-
-.plan-price {
-    margin-top: 60px;
-    border: solid 1px #b5b5b5;
-    border-radius: 10px;
-    background-color: #f0f4f8;
-    text-align: center;
-}
-
-.plan-h3 {
-    font-size: 20px;
-    line-height: 1.75;
-
-    @media (max-width: 490px) {
-        font-size: 16px;
-    }
-}
-
-.plan-payment {
-    padding: 32px;
-
-    @media (max-width: 490px) {
-        font-size: 14px;
-    }
-}
-
-.back-btn {
-    display: flex;
-}
-
-.plan-name {
-    font-family: 'Roboto';
-}
-
-.plan-info {
-    font-weight: bold;
-    font-family: 'Roboto';
-}
+.plan
+    padding 45px
+    background #ffffff
+.plan-heading
+    font-size 18px
+    @media (max-width: 490px)
+        font-size 16px
+.plan-details
+    display flex
+    flex-direction column
+    p
+        margin-bottom 25px
+        font-weight bold
+        font-size 16px
+        line-height 1.75
+        @media (max-width: 490px)
+            font-size 14px
+.plan-box
+    display inline-block
+    max-width 100%
+.plan-inner
+    padding 60px 50px
+    border-radius 10px
+    background-color #ffffff
+    box-shadow 0 0 6px 0 rgba(0, 0, 0, 0.16)
+    @media (max-width: 490px)
+        padding 50px
+.plan-heading
+    display flex
+    flex-direction column
+    justify-content center
+    align-items center
+    h3
+        font-size 18px
+        @media (max-width: 490px)
+            font-size 14px
+.plan-divider
+    height 2px
+    background-color #e7e8ea
+.plan-config
+    display flex
+    flex-direction row
+    justify-content space-around
+    align-items center
+    margin-top 25px
+.plan-information
+    display flex
+    flex-direction column
+    justify-content space-around
+    align-items center
+    font-size 16px
+    @media (max-width: 490px)
+        font-size 12px
+.plan-information>span+span
+    margin-top 10px
+.plan-information+.plan-information
+    margin-right 10px
+.plan-image img
+    width 30px
+    @media (max-width: 490px)
+        width 24px
+.plan-price
+    margin-top 60px
+    border solid 1px #b5b5b5
+    border-radius 10px
+    background-color #f0f4f8
+    text-align center
+.plan-h3
+    font-size 20px
+    line-height 1.75
+    @media (max-width: 490px)
+        font-size 16px
+.plan-payment
+    padding 32px
+    @media (max-width: 490px)
+        font-size 14px
+.back-btn
+    display flex
+.plan-name
+    font-family 'Roboto'
+.plan-info
+    font-weight bold
+    font-family 'Roboto'
 </style>
