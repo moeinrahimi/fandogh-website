22e7bc987770925529fbcd4da5ad7ce15ae5fd10
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2018-11-10 19:09:50 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,6 +30,7 @@
     },
   }
 </script>
+<script type="text/javascript">!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="b34779ab-3e49-4256-8f71-ec8ae7e76d64";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();</script>
 
 <style lang="stylus" scoped>
   @import '../assets/css/main.styl'
