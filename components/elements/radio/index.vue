695eed5d1bdebfeb3a1f73d630c361ce39ee4c6c
reza <shoosh2007@gmail.com>
2018-11-12 14:00:33 +0330

    implement radio component

---
+++ components/elements/radio/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="radio">
+      <div class="radio-label">
+          {{title}}
+      </div>
+      <div v-for="option in options" class="radio-option">
+          <f-radio @onSelect="selectOption" :selected="selected" :title="option"></f-radio>
+      </div>
+  </div>
+</template>
+
+<script>
+  import FRadio from './radio'
+  export default {
+    props:['title', 'options'],
+    data(){
+      return {
+        selected: this.options[0]
+      }
+    },
+    components:{
+      FRadio
+    },
+    methods:{
+      selectOption(title){
+        this.selected = title
+        this.$emit('input', title)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+.radio
+    display: flex
+    justify-content right
+</style>
---
+++ components/elements/radio/radio.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="radio-option">
+      <div @click="onSelect" class="radio-option-circle">
+          <div v-if="isSelected" class="radio-selected">
+
+          </div>
+      </div>
+      <div class="radio-option-title">
+          {{title}}
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['selected', 'title'],
+    computed: {
+      isSelected(){
+        return this.title === this.selected
+      }
+    },
+    methods:{
+      onSelect(){
+        this.$emit('onSelect', this.title)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+    .radio-option
+        display flex
+        flex-flow wrap row
+        &-option-title
+            font-family: IRANYekan
+            font-size: 16px
+        &-circle
+                border 1px solid #00e3dc
+                border-radius 50%
+                background #fff
+                width 16px
+                height 16px
+                display flex
+                align-items center
+                justify-content center
+                .radio-selected
+                    border 1px solid #00e3dc
+                    border-radius 50%
+                    background #00e3dc
+                    width 10px
+                    height 10px
+</style>
